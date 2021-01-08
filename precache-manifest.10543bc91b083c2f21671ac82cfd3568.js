bd2f894b6495eed36368a6164fdfc711e72f6e29
Gabriel Adrian Juarez de la Cruz <bicosind@gmail.com>
2021-01-08 00:13:17 -0600

    Updates

---
+++ precache-manifest.10543bc91b083c2f21671ac82cfd3568.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08b894467e9410b2967277c77ad4e0ef",
+    "url": "/my_portafolio/index.html"
+  },
+  {
+    "revision": "868e5f06d5c37124767b",
+    "url": "/my_portafolio/static/css/2.5351225d.chunk.css"
+  },
+  {
+    "revision": "b93b0cf20addeb1d2005",
+    "url": "/my_portafolio/static/css/main.36a51fcf.chunk.css"
+  },
+  {
+    "revision": "868e5f06d5c37124767b",
+    "url": "/my_portafolio/static/js/2.effc0753.chunk.js"
+  },
+  {
+    "revision": "e9875d7c1187f4f0558e7fbac051f48d",
+    "url": "/my_portafolio/static/js/2.effc0753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b93b0cf20addeb1d2005",
+    "url": "/my_portafolio/static/js/main.b1a3c93e.chunk.js"
+  },
+  {
+    "revision": "4cf3f1dc1111c005ee46",
+    "url": "/my_portafolio/static/js/runtime-main.1f466d3d.js"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/my_portafolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/my_portafolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/my_portafolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/my_portafolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/my_portafolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/my_portafolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c8336b0d63645b325ab15b2f0962c41e",
+    "url": "/my_portafolio/static/media/image1.c8336b0d.jpg"
+  },
+  {
+    "revision": "59b9d279982432ef311d61ad3f99df7e",
+    "url": "/my_portafolio/static/media/image2.59b9d279.jpg"
+  },
+  {
+    "revision": "2a6b4ad51cf22327099fa86ddc25e760",
+    "url": "/my_portafolio/static/media/image3.2a6b4ad5.jpg"
+  },
+  {
+    "revision": "08c89a57d218a16e1c11179d4eb5721b",
+    "url": "/my_portafolio/static/media/image4.08c89a57.jpg"
+  },
+  {
+    "revision": "8aca013ddcb49d183bdd9ebdf1358af3",
+    "url": "/my_portafolio/static/media/image5.8aca013d.jpg"
+  },
+  {
+    "revision": "5bbd1261456e19bf57ea90167f2c73dc",
+    "url": "/my_portafolio/static/media/image6.5bbd1261.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/my_portafolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/my_portafolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/my_portafolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/my_portafolio/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/my_portafolio/static/media/myImage.bd80794f.png"
+  }
+]);
